f7dbc5fe4f04607bf89de6c8c18bb0919c8de9f3
Pedro Carneiro <pedrohlc@usp.br>
2026-04-23 13:40:08 -0300

    feat: PaymentListener implemented

---
+++ src/main/java/com/example/demo/consumer/PaymentListener.java
@@ -0,0 +1,37 @@
+package com.example.demo.consumer;
+
+
+import com.example.demo.dto.AccountRequestDTO;
+import com.example.demo.model.Account;
+import com.example.demo.repository.AccountRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Component
+public class PaymentListener {
+
+
+    private final AccountRepository repository;
+
+    public PaymentListener(AccountRepository repository){
+        this.repository = repository;
+    }
+
+    @KafkaListener(topics = "payments-topic", groupId = "payments-group")
+    public void receive(AccountRequestDTO data){
+        System.out.println(">>> Kafka Recebeu" + data.ownerName());
+
+        Account entity = new Account();
+        entity.setOwnerName(data.ownerName());
+        entity.setAdress(data.Adress());
+        entity.setTel(data.Tel());
+
+        repository.save(entity);
+        System.out.println(">>> Gravado no banco");
+    }
+
+}
